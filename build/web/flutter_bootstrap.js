ed30a4a559823907b3d3d3a9c3674f5e22ad34c2
Daniel Peruffo <d.peruffo@gmail.com>
2026-07-19 09:28:25 -0300

    build: tela Financeiro

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4150760143" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3613730286" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
